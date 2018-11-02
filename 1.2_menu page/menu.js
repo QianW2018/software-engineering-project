a85f90092cb22dcc70303eb21ae1d017258bc2ef
QianW2018 <43798833+QianW2018@users.noreply.github.com>
2018-11-02 12:30:08 -0400

    Add files via upload

---
+++ 1.2_menu page/menu.js
@@ -0,0 +1,212 @@
+
+function placeOrder(){
+    window.open("delivery_address.html");
+}
+    
+    
+function plusButton1(){
+
+    var i=0;
+    i=document.getElementById("qtyInput01").value;
+    i=Number(i);
+    i++;
+    document.getElementById("qtyInput01").value = i;
+    
+}
+    
+function minusButton1(){
+
+    var i=0;
+    i=document.getElementById("qtyInput01").value;
+    i=Number(i);
+    i--;
+    if (i<0){
+        i=0;
+    }
+    document.getElementById("qtyInput01").value = i;
+    
+}
+
+function plusButton2(){
+
+    var i=0;
+    i=document.getElementById("qtyInput02").value;
+    i=Number(i);
+    i++;
+    document.getElementById("qtyInput02").value = i;
+    
+}
+    
+function minusButton2(){
+
+    var i=0;
+    i=document.getElementById("qtyInput02").value;
+    i=Number(i);
+    i--;
+    if (i<0){
+        i=0;
+    }
+    document.getElementById("qtyInput02").value = i;
+    
+
+
+function plusButton3(){
+
+    var i=0;
+    i=document.getElementById("qtyInput03").value;
+    i=Number(i);
+    i++;
+    document.getElementById("qtyInput03").value = i;
+    
+}
+    
+function minusButton3(){
+
+    var i=0;
+    i=document.getElementById("qtyInput03").value;
+    i=Number(i);
+    i--;
+    if (i<0){
+        i=0;
+    }
+    document.getElementById("qtyInput03").value = i;
+    
+}
+
+function plusButton4(){
+
+    var i=0;
+    i=document.getElementById("qtyInput04").value;
+    i=Number(i);
+    i++;
+    document.getElementById("qtyInput04").value = i;
+    
+}
+    
+function minusButton4(){
+
+    var i=0;
+    i=document.getElementById("qtyInput04").value;
+    i=Number(i);
+    i--;
+    if (i<0){
+        i=0;
+    }
+    document.getElementById("qtyInput04").value = i;
+    
+}
+
+function plusButton5(){
+
+    var i=0;
+    i=document.getElementById("qtyInput05").value;
+    i=Number(i);
+    i++;
+    document.getElementById("qtyInput05").value = i;
+    
+}
+    
+function minusButton5(){
+
+    var i=0;
+    i=document.getElementById("qtyInput05").value;
+    i=Number(i);
+    i--;
+    if (i<0){
+        i=0;
+    }
+    document.getElementById("qtyInput05").value = i;
+    
+}
+
+function plusButton6(){
+
+    var i=0;
+    i=document.getElementById("qtyInput06").value;
+    i=Number(i);
+    i++;
+    document.getElementById("qtyInput06").value = i;
+    
+}
+    
+function minusButton6(){
+
+    var i=0;
+    i=document.getElementById("qtyInput06").value;
+    i=Number(i);
+    i--;
+    if (i<0){
+        i=0;
+    }
+    document.getElementById("qtyInput06").value = i;
+    
+}
+
+function plusButton7(){
+
+    var i=0;
+    i=document.getElementById("qtyInput07").value;
+    i=Number(i);
+    i++;
+    document.getElementById("qtyInput07").value = i;
+    
+}
+    
+function minusButton7(){
+
+    var i=0;
+    i=document.getElementById("qtyInput07").value;
+    i=Number(i);
+    i--;
+    if (i<0){
+        i=0;
+    }
+    document.getElementById("qtyInput07").value = i;
+    
+}	
+
+function plusButton8(){
+
+    var i=0;
+    i=document.getElementById("qtyInput08").value;
+    i=Number(i);
+    i++;
+    document.getElementById("qtyInput08").value = i;
+    
+}
+    
+function minusButton8(){
+
+    var i=0;
+    i=document.getElementById("qtyInput08").value;
+    i=Number(i);
+    i--;
+    if (i<0){
+        i=0;
+    }
+    document.getElementById("qtyInput08").value = i;
+    
+}
+
+function plusButton9(){
+
+    var i=0;
+    i=document.getElementById("qtyInput09").value;
+    i=Number(i);
+    i++;
+    document.getElementById("qtyInput09").value = i;
+    
+}
+    
+function minusButton9(){
+
+    var i=0;
+    i=document.getElementById("qtyInput09").value;
+    i=Number(i);
+    i--;
+    if (i<0){
+        i=0;
+    }
+    document.getElementById("qtyInput09").value = i;
+    
+}	
